728d2c5fc5fb4d261c0e8c3fed034c4b370f34e5
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2025-04-27 10:19:28 +0530

    intial setup of next js

---
+++ quiz/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1>
+          Quiz Platform
+        </h1>
+      </div>
+    </>
+  );
+}
